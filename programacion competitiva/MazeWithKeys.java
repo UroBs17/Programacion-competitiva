51d7b86d8d67a00674e72d12d9fc0f729387e18b
Michael Preciado <43444290+UroBs17@users.noreply.github.com>
2019-02-13 22:14:47 -0500

    Update MazeWithKeys.java

--- programacion competitiva/MazeWithKeys.java
+++ programacion competitiva/MazeWithKeys.java
@@ -26,24 +26,16 @@ public class MazeWithKeys {
 		Deque<Pair> s = new LinkedList<Pair>();
 		s.push(new Pair(x,y));
 		while(!s.isEmpty()) {
-			//System.out.println(s.toString());
-			//System.out.println("Llaves: "+keys.toString());
-			//System.out.println("Puertas: "+doors.toString());
 			Pair top = s.removeFirst();
-			//System.out.println(top.x+" "+top.y);
 			vi[top.x][top.y]=true;
 			if(matriz[top.x][top.y]=='*') {
-				//System.out.println("Hola que tal chavalada"+matriz[top.x][top.y]+" "+top.x+" "+top.y);
 				return true&&abripuertasjajasalu2;
 			}
 			for(int i = 0; i<4; i++) {
 				if((top.x+posicionesX[i]>=0 && top.x+posicionesX[i]<matriz.length) && (top.y+posicionesY[i]>=0 && top.y+posicionesY[i]<matriz[0].length)) {
-					//System.out.println(matriz[top.x+posicionesX[i]][top.y+posicionesY[i]]+" booleano de visitado: "+vi[top.x+posicionesX[i]][top.y+posicionesY[i]]);
 					if(!vi[top.x+posicionesX[i]][top.y+posicionesY[i]]) {
 						if(matriz[top.x+posicionesX[i]][top.y+posicionesY[i]]>='a' && matriz[top.x+posicionesX[i]][top.y+posicionesY[i]]<='z') {
-							//System.out.println("Soy la llave : "+matriz[top.x+posicionesX[i]][top.y+posicionesY[i]]);
 							if(doors.containsKey((char)(matriz[top.x+posicionesX[i]][top.y+posicionesY[i]]-32))) {
-								//System.out.println("Como llave, encontré una puerta: "+doors.toString());
 								abripuertasjajasalu2 = true;
 								Pair temp = doors.get((char)(matriz[top.x+posicionesX[i]][top.y+posicionesY[i]]-32));
 								s.push(temp);
@@ -54,9 +46,7 @@ public class MazeWithKeys {
 							}
 						}
 						else if(matriz[top.x+posicionesX[i]][top.y+posicionesY[i]]>='A' && matriz[top.x+posicionesX[i]][top.y+posicionesY[i]]<='Z') {
-							//System.out.println("Soy la puerta: "+matriz[top.x+posicionesX[i]][top.y+posicionesY[i]]);
 							if(keys.containsKey((char)(matriz[top.x+posicionesX[i]][top.y+posicionesY[i]]+32))) {
-								//System.out.println("Como puerta, encontré una llave: "+keys.toString());
 								abripuertasjajasalu2 = true;
 								Pair temp = new Pair(top.x+posicionesX[i],top.y+posicionesY[i]);
 								s.push(temp);
@@ -79,10 +69,6 @@ public class MazeWithKeys {
 		walls.put((char)(c+'0'), 1);
 		s.push(new Pair(x,y));
 		while(!s.isEmpty()) {
-			/*for(boolean[] b : vi) {
-			System.out.println(Arrays.toString(b));
-			}
-			System.out.println("/");*/
 			Pair top = s.removeFirst();
 			vi[top.x][top.y]=true;
 			matriz[top.x][top.y]=((matriz[top.x][top.y]>='a' && matriz[top.x][top.y]<='z') )?matriz[top.x][top.y]:(char)(c+'0');
@@ -133,7 +119,6 @@ public class MazeWithKeys {
 		}
 		
 		vi = new boolean[a.length][a[0].length()];
-		//System.out.println(solveDelSolve.toString());
 		int cnt=0;
 		for(int i = 0; i < matriz.length; i++) {
 			for(int j = 0; j<matriz[0].length;j++) {
@@ -146,34 +131,7 @@ public class MazeWithKeys {
 			}
 			
 		}
-		
-		//System.out.println(cnt+" /// Solución");
-		/*for(char[] chl : matriz) {
-			for(char ch : chl ) {
-				System.out.print(ch+" ");
-			}
-			System.out.println();
-		}*/
 		return cnt;
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	/*public static void main(String[] args) {
-		String[] a ={"#kD#.n.#.#.#u.#.#.dw", "j#h##.##....#L#.#v.#", ".J..#u#.##.##s#o##..", "#.#rEB#..mTzNI#.....", ".###.###b#p##k#w###.", "p.#..q..##i.##a.c.#.", "#e..#w#sfn.#d##.a#.R", "F##.###l##t.sb##.###", "G.k##.h.h#..#x#.iO.i", ".#lrct#.##.###..##.#", "##y##.#.y###.##.K#pr", "j.nC.##b#...wm#g####", "v###Z.a##z#U#lwhc.#a", ".#s###zfu.#yz####lWg", "##o#X##.#..#.y*M##q.", "..iH.#ht####.##z#.r#", ".#.#Y#u..d#.f#..###.", "##.#..Q#####.#e##...", ".V.##.#.o#Si.#..Aj##", "#...#...#P.#.#..#p.c"};
-		String[] b ={"a#a#*", "#..#.", "a..A."};
-		String[] c ={"..*", ".a.", "A.."};
-		String[] d ={"a#a#*", "#..#.", "a..A."};
-		String[] e ={"..*", ".a.", "A.."};
-		String[] f ={"a#a#*", "#..#.", "a..A."};
-		MazeWithKeys mwk = new MazeWithKeys();
-		mwk.startingPoints(a);
-		mwk.startingPoints(b);
-	}*/
 
 }
