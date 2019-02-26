381c402caeffc05ba879859da1bbca863ec12271
UroBs17 <michael.preciado@mail.escuelaing.edu.co>
2019-02-26 18:06:21 -0500

    clean

--- daea/recursion/10970.py
+++
@@ -1,9 +0,0 @@
-from sys import stdin
-
-def main():
-    while True:
-        line=stdin.readline().strip()
-        if(len(line)==0):break
-        a,b=[int(x) for x in line.split()]
-        print(a*b-1)
-main()
