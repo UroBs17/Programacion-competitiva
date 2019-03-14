90bb68cfabe1bbce447b8054ab1a1c0e71bf782d
Michael Preciado <43444290+UroBs17@users.noreply.github.com>
2019-03-13 21:25:16 -0500

    Update primeFactors.java

--- cuadernillo/Math/Number theory/primeFactors.java
+++ cuadernillo/Math/Number theory/primeFactors.java
@@ -6,7 +6,7 @@ public static void primeFactors(long M) {
 		long numberPF=0;
 		long numDiv=1;
 		long sumDiv=1;
-		double x=1.0;//si x=2 la sumdiv guardara la suma de los divisores al cuadrado si es 3 al cubo ...
+		double x=1.0;//sum(div^2)||sum(div^3)...
 		while(1l * p * p <= N){
 			if(N % p == 0){
 				numberPF++;
@@ -15,13 +15,15 @@ public static void primeFactors(long M) {
 			int power = 0;
 			while(N % p == 0) { N /= p; ++power; }
 			numDiv *= (power + 1);
-			sumDiv *= ((long)Math.pow((double)p, (power + 1.0)*x) - 1) / ((long)Math.pow((double)p,x) - 1); 
+			sumDiv *= ((long)Math.pow((double)p, (power + 1.0)*x) - 1) 
+			sumDiv /= ((long)Math.pow((double)p,x) - 1); 
 			p = primes.get(++idx);
 		}
 		if(N != 1){
 			numberPF++;
 			numberCP -= numberCP / N;
 			numDiv <<= 1;
-			sumDiv *= ((long)Math.pow((double)p, 2.0 * x) - 1) / ((long)Math.pow((double)p,x) - 1);
+			sumDiv *= ((long)Math.pow((double)p, 2.0 * x) - 1) 
+			sumDiv /= ((long)Math.pow((double)p,x) - 1);
 		}
 }
