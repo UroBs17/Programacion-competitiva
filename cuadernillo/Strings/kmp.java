4294b737f1eb32a8f20e910808b71550f8d1f905
Michael Preciado <43444290+UroBs17@users.noreply.github.com>
2019-05-10 09:38:32 -0500

    Update kmp.java

--- cuadernillo/Strings/kmp.java
+++ cuadernillo/Strings/kmp.java
@@ -1,24 +1,33 @@
-public static char[] T,P;
-public static int[] b;
-public static int n,m;
-public static final int MAX = 400005;
-public static void kmpPreprocess(){
-       int j = -1;
-       for(int i = 0; i < m; i++){
-       	       if(j>=0 && P[i]==P[j]) b[i]=b[j];
-	       else b[i]=j;
-	       while(j>=0 && P[i]!=P[j]) j=b[j];
-	       j++;
+static int search(String pattern, String text) {
+	    int[] lsp = computeLspTable(pattern);
+	    
+	    int j = 0;  // Number of chars matched in pattern
+	    for (int i = 0; i < text.length(); i++) {
+	        while (j > 0 && text.charAt(i) != pattern.charAt(j)) {
+	            // Fall back in the pattern
+	            j = lsp[j - 1];  // Strictly decreasing
+	        }
+	        if (text.charAt(i) == pattern.charAt(j)) {
+	            // Next char matched, increment position
+	            j++;
+	            if (j == pattern.length())
+	                return i - (j - 1);
+	        }
+	    }
+	    
+	    return -1;  // Not found
 	}
-}
-public static int kmpSearch(){
-       	int j = 0;
-	int i = 0;
-	int cont = 0;
-	while(i < n){
-		while(j>=0 && T[i]!=P[j]) j = b[j];
-		i++;j++;
-		if(j==m){j=b[j];return i-j}
+	static int[] computeLspTable(String pattern) {
+	    int[] lsp = new int[pattern.length()];
+	    lsp[0] = 0;  // Base case
+	    for (int i = 1; i < pattern.length(); i++) {
+	        // Start by assuming we're extending the previous LSP
+	        int j = lsp[i - 1];
+	        while (j > 0 && pattern.charAt(i) != pattern.charAt(j))
+	            j = lsp[j - 1];
+	        if (pattern.charAt(i) == pattern.charAt(j))
+	            j++;
+	        lsp[i] = j;
+	    }
+	    return lsp;
 	}
-	return -1;
-}
