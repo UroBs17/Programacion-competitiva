60a43b7992a5164ffacaf4230035e1fc95836e0d
Michael Preciado <43444290+UroBs17@users.noreply.github.com>
2019-05-10 10:08:36 -0500

    Create SUBST1_spoj.java

---
+++ ejercicios/Strings/SUBST1_spoj.java
@@ -0,0 +1,79 @@
+import java.io.*;
+import java.util.*;
+
+public class SUBST1_spoj {
+	static int[] RA, tempRA, SA,tempSA, c,phi,LCP,PLCP;;
+	static int n;
+	static char[] T;
+  static void radixSort(int k){
+		int sum,maxi = Math.max(n, 300);
+		c = new int[100010];
+		tempRA=new int[n];
+		tempSA=new int[n];
+		for(int i = 0; i < n; i++){
+			c[i+k<n ? RA[i+k] : 0]++;
+		}
+		for(int i = sum = 0; i < maxi; i++){
+			int t = c[i];
+			c[i] = sum;
+			sum+=t;
+		}
+		for(int i = 0; i < n; i++)
+			tempSA[c[SA[i]+k < n ?  RA[SA[i]+k]: 0]++] = SA[i];
+		for(int i=0; i < n; i++)
+			SA[i] = tempSA[i];
+	}
+ 
+	static void buildSA(){
+		int r;
+		RA=new int[n];
+		SA=new int[n];
+		for(int i = 0; i < n; i++) RA[i] = T[i];
+		for(int  i = 0; i < n; i++) SA[i] = i;
+		for(int k = 1; k < n; k<<=1){
+			radixSort(k);
+			radixSort(0);
+			tempRA[SA[0]] = r= 0;
+			for(int i = 1; i < n; i++)
+				tempRA[SA[i]] = (RA[SA[i]] == RA[SA[i-1]] && RA[SA[i]+k] == RA[SA[i-1]+k]) ? r : ++r;
+			for(int i = 0; i <n; i++)
+				RA[i] = tempRA[i];
+			if (RA[SA[n-1]] == n-1) break;
+		}
+	}
+ 
+	static void LCP(){
+		int L;
+		phi = new int[n];
+		phi[SA[0]] = -1;
+		LCP=new int[n];
+		PLCP=new int[n];
+		for(int i = 1; i < n; i++)
+			phi[SA[i]] = SA[i-1];
+ 
+		for(int i = L=0; i <n;i++){
+			if (phi[i] == -1) {PLCP[i] = 0; continue;}
+			while(T[i+L] == T[phi[i] + L]) L++;
+			PLCP[i] = L;
+			L = Math.max(L-1,0);
+		}
+		for(int i = 0; i < n; i++)
+			LCP[i] = PLCP[SA[i]];
+	}
+	public static void main(String[] args) throws Exception{
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		int c=Integer.parseInt(br.readLine());
+		while(c-->0) {
+			T=(br.readLine()+"$").toCharArray();
+			n=T.length;
+			buildSA();
+			LCP();
+			int s = 0;
+	        for (int i = 0; i < n; i++){
+	            s += n - SA[i] - 1;
+	            s -= LCP[i];
+	        }
+			System.out.println(s);
+		}
+	}
+}
